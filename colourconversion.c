c8c4d01f1c0059cc81c15aa2557160e7f284e047
Tal Melamed <talm@b133.seng.uvic.ca>
2018-07-09 17:11:37 -0700

    just starting file

---
+++ colourconversion.c
@@ -0,0 +1,24 @@
+float r[2][2] = {{0.5,0.5},
+		{0.2,0.2}};
+float **g[2][2] = {{0.7,1.0},
+		{0.2,0.5}};
+float **b[2][2] = {{0.0,0.0},
+		{0.4,0.4}};
+float **y[2][2];
+float **cr[1][1];
+float **cb[1][1];
+
+void RGBtoYCC(int width, int length, float **r, float **g, float **b, float **y, float **cr, float **cb){
+
+}
+
+void YCCtoRGB(float **r, float **g, float **b, float **y, float **cr, float **cb){
+
+}
+
+int main(void){
+		
+}
+
+
+
