da3049132f976f5489ec1dd77484e4cf02382799
talamalopilis <tal.melamed.69@gmail.com>
2018-07-22 02:21:26 -0700

    integer arithmetic for rgb>ycc, y works

--- functionalsoftware.c
+++ functionalsoftware.c
@@ -3,33 +3,31 @@
 #define cl 1
 #define cw 2
 #include <stdio.h>
-unsigned char r[l][w] = {{128,128,128,128},
-		{128,128,128,128}};
-unsigned char g[l][w] = {{128,128,128,128},
-		{128,128,128,128}};
-unsigned char b[l][w] = {{0,0, 0, 0},
-		{0,0, 0, 0}};
-unsigned char y[l][w];
-unsigned char cr[cl][cw];
-unsigned char cb[cl][cw];
+float r[l][w] = {{128.0,128.0,128.0,128.0},
+		{128.0,128.0,128.0,128.0}};
+float g[l][w] = {{128.0,128.0,128.0,128.0},
+		{128.0,128.0,128.0,128.0}};
+float b[l][w] = {{0.0,0.0, 0.0, 0.0},
+		{0.0,0.0, 0.0, 0.0}};
+float y[l][w];
+float cr[cl][cw];
+float cb[cl][cw];
 
-void RGBtoYCC(unsigned char r[l][w], unsigned char g[l][w], unsigned char b[l][w], unsigned char y[l][w], unsigned char cr[cl][cw], unsigned char cb[cl][cw]){
+void RGBtoYCC(float r[l][w], float g[l][w], float b[l][w], float y[l][w], float cr[cl][cw], float cb[cl][cw]){
 	int i, j;
-	unsigned int rval, gval, bval, crtmp = 0, cbtmp = 0;
+	float rval, gval, bval;
 	for (i = 0; i < l; i++){
 		for (j = 0; j < w; j++){
-			//scale to 16 bit
-			rval = (int) r[i][j] << 8;
-			gval = (int) g[i][j] << 8;
-			bval = (int) b[i][j] << 8;
-			y[i][j] = 4096 + (16843*rval + 33030*gval + 6423*bval >> 16) + 128 >> 8;
-			crtmp += 32768 + (28770*rval - 24117*gval - 4653*bval >> 16) + 128 >> 8;
-			cbtmp += 32768 - (9699*rval - 19071*gval + 28770*bval >> 16) + 128 >> 8;
+			rval = r[i][j];
+			gval = g[i][j];
+			bval = b[i][j];
+			y[i][j] = 16.0 + 0.257*rval + 0.504*gval + 0.098*bval;
+			cr[i/2][j/2] += 128.0 + 0.439*rval - 0.368*gval - 0.071*bval;
+			cb[i/2][j/2] += 128.0 - 0.148*rval - 0.291*gval + 0.439*bval;
 
 			if (i % 2 == 1 && j % 2 == 1){
-				cr[i/2][j/2] = crtmp/4;
-				cb[i/2][j/2] = cbtmp/4;
-				crtmp = cbtmp = 0;
+				cr[i/2][j/2] = cr[i/2][j/2]/4;
+				cb[i/2][j/2] = cb[i/2][j/2]/4;
 			}
 		}
 	}	
@@ -67,11 +65,11 @@ void print_values(float r[l][w],float g[l][w],float b[l][w])
 	}
 }
 int main(void){
-//	print_values(r,g,b);
+	print_values(r,g,b);
 	RGBtoYCC(r,g,b,y,cr,cb);
-	//YCCtoRGB(r,g,b,y,cr,cb);
-//	printf("\n");
-//	print_values(r,g,b);
+	YCCtoRGB(r,g,b,y,cr,cb);
+	printf("\n");
+	print_values(r,g,b);
 	return 0;
 }
 
